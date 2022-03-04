fd23c80e13a16366356b106d899f27fb361d9f43
Patrick Kenyon <patrick.kenyon@playsportsnetwork.com>
2022-03-04 16:59:53 +0000

    add task status patch method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,7 +13,7 @@ export class TasksService {
     return this.tasks;
   }
 
-  getTaskById(id: string) {
+  getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
   }
 
@@ -33,5 +33,11 @@ export class TasksService {
 
   deleteTaskById(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
   }
 }
